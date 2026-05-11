13b993b47625c0c098c6d43b2830bcfa1ae14d50
gittoMoota <giftmoota04@gmail.com>
2026-05-11 11:03:22 +0200

    Original-Reviewer method call (T6)

--- Original/src/Reviewer.py
+++ Original/src/Reviewer.py
@@ -1,10 +1,10 @@
+from metrics import call_counter
+import metrics
 class Reviewer:
     def save_score(self, score):
-        global call_counter
-        call_counter += 1
+        metrics.call_counter += 1
         print(f"Reviewer: Saving score {score}...")
 
     def submit_score(self, score):
-        global call_counter
-        call_counter += 1
+        metrics.call_counter += 1
         print(f"Reviewer: Submitting score {score}...")
